7f98f129726e743358d6d86bec6c8d794b4d589a
Alex David <davidalexrobertyt@gmail.com>
2025-05-21 22:41:06 +0300

    Implement Date class

--- model/Date.cpp
+++ model/Date.cpp
@@ -2,4 +2,45 @@
 // Created by AleXutzZu on 21/05/2025.
 //
 
+#include <stdexcept>
 #include "Date.h"
+
+Date::Date(unsigned int year, unsigned int month, unsigned int day) : year(year), month(month), day(day) {
+    if (month < 1 || month > 12) {
+        throw std::invalid_argument("Invalid month: " + std::to_string(month));
+    }
+
+    if (year < 1) {
+        throw std::invalid_argument("Invalid year: " + std::to_string(year));
+    }
+
+    unsigned int maxDay = daysInMonth(month, year);
+    if (day < 1 || day > maxDay) {
+        throw std::invalid_argument("Invalid day: " + std::to_string(day) +
+                                    " for month " + std::to_string(month) +
+                                    " in year " + std::to_string(year));
+    }
+}
+
+unsigned int Date::getYear() const {
+    return year;
+}
+
+unsigned int Date::getMonth() const {
+    return month;
+}
+
+unsigned int Date::getDay() const {
+    return day;
+}
+
+Date::Date() : year(2000), day(1), month(1) {}
+
+std::ostream &operator<<(std::ostream &os, const Date &date) {
+    return os << date.day << "/" << date.month << "/" << date.year;
+}
+
+std::istream &operator>>(std::istream &is, Date &date) {
+    char separator;
+    return is >> date.day >> separator >> date.month >> separator >> date.year;
+}
--- model/Date.h
+++ model/Date.h
@@ -6,9 +6,53 @@
 #define OOP_PROJECT_LIBRARYMANAGEMENT_DATE_H
 
 
+#include <ostream>
+#include <istream>
+
 class Date {
 private:
     unsigned int year, month, day;
+
+    static bool isLeapYear(unsigned int y) {
+        return (y % 4 == 0 && y % 100 != 0) || (y % 400 == 0);
+    }
+
+    static unsigned int daysInMonth(unsigned m, unsigned int y) {
+        switch (m) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                return 31;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                return 30;
+            case 2:
+                return isLeapYear(y) ? 29 : 28;
+            default:
+                return 0;
+        }
+    }
+
+public:
+    Date();
+
+    Date(unsigned int year, unsigned int month, unsigned int day);
+
+    [[nodiscard]] unsigned int getYear() const;
+
+    [[nodiscard]] unsigned int getMonth() const;
+
+    [[nodiscard]] unsigned int getDay() const;
+
+    friend std::ostream &operator<<(std::ostream &os, const Date &date);
+
+    friend std::istream &operator>>(std::istream &is, Date &date);
 };
 
 
