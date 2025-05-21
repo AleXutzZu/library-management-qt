f3ecbd07db39a904fe352f58b885200f847bd149
Alex David <davidalexrobertyt@gmail.com>
2025-05-21 20:09:46 +0300

    Update base Publication class with base fields

--- model/Publication.cpp
+++ model/Publication.cpp
@@ -3,3 +3,20 @@
 //
 
 #include "Publication.h"
+
+#include <utility>
+
+Publication::Publication(const std::vector<std::string> &authors, std::string title, const Date &publicationDate)
+        : authors(authors), title(std::move(title)), publicationDate(publicationDate) {}
+
+const std::vector<std::string> &Publication::getAuthors() const {
+    return authors;
+}
+
+const std::string &Publication::getTitle() const {
+    return title;
+}
+
+const Date &Publication::getPublicationDate() const {
+    return publicationDate;
+}
--- model/Publication.h
+++ model/Publication.h
@@ -5,9 +5,25 @@
 #ifndef OOP_PROJECT_LIBRARYMANAGEMENT_PUBLICATION_H
 #define OOP_PROJECT_LIBRARYMANAGEMENT_PUBLICATION_H
 
+#include <string>
+#include <vector>
+#include "Date.h"
 
 class Publication {
+protected:
+    std::vector<std::string> authors;
+    std::string title;
+    Date publicationDate;
 
+public:
+
+    Publication(const std::vector<std::string> &authors, std::string title, const Date &publicationDate);
+
+    [[nodiscard]] const std::vector<std::string> &getAuthors() const;
+
+    [[nodiscard]] const std::string &getTitle() const;
+
+    [[nodiscard]] const Date &getPublicationDate() const;
 };
 
 
