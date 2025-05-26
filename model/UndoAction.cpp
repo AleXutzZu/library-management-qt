119916b4cbf106ea52e04e2e02a0642a29cee05c
Alex David <davidalexrobertyt@gmail.com>
2025-05-27 01:05:44 +0300

    Implement Command design pattern for undo operations

---
+++ model/UndoAction.cpp
@@ -0,0 +1,22 @@
+//
+// Created by AleXutzZu on 27/05/2025.
+//
+
+#include "UndoAction.h"
+
+UndoAction::UndoAction(BasePublicationRepository &repository, const std::shared_ptr<Publication> &item) : repository(
+        repository), item(item) {}
+
+void UndoAdd::executeUndo() {
+    repository.removeByTitle(item->getTitle());
+}
+
+UndoAdd::UndoAdd(BasePublicationRepository &repository, const std::shared_ptr<Publication> &item) : UndoAction(
+        repository, item) {}
+
+void UndoRemove::executeUndo() {
+    repository.add(item);
+}
+
+UndoRemove::UndoRemove(BasePublicationRepository &repository, const std::shared_ptr<Publication> &item) : UndoAction(
+        repository, item) {}
---
+++ model/UndoAction.h
@@ -0,0 +1,39 @@
+//
+// Created by AleXutzZu on 27/05/2025.
+//
+
+#ifndef OOP_PROJECT_LIBRARYMANAGEMENT_UNDOACTION_H
+#define OOP_PROJECT_LIBRARYMANAGEMENT_UNDOACTION_H
+
+
+#include "../repository/BasePublicationRepository.h"
+
+class UndoAction {
+protected:
+    BasePublicationRepository &repository;
+    std::shared_ptr<Publication> item;
+
+public:
+    virtual void executeUndo() = 0;
+
+    UndoAction(BasePublicationRepository &repository, const std::shared_ptr<Publication> &item);
+
+    virtual ~UndoAction() = default;
+};
+
+class UndoAdd: public UndoAction {
+
+public:
+    UndoAdd(BasePublicationRepository &repository, const std::shared_ptr<Publication> &item);
+
+    void executeUndo() override;
+};
+
+class UndoRemove: public UndoAction{
+public:
+    UndoRemove(BasePublicationRepository &repository, const std::shared_ptr<Publication> &item);
+
+    void executeUndo() override;
+};
+
+#endif //OOP_PROJECT_LIBRARYMANAGEMENT_UNDOACTION_H
