99d4a5122724eb8d37573f9a18f70bf2b182dc7a
Alex David <davidalexrobertyt@gmail.com>
2025-05-29 15:47:21 +0300

    Implement delete action button for the delegate

--- ui/ActionButtonsDelegate.cpp
+++ ui/ActionButtonsDelegate.cpp
@@ -3,13 +3,30 @@
 //
 
 #include "ActionButtonsDelegate.h"
+#include "PublicationTableModel.h"
+#include <QApplication>
 
 void
 ActionButtonsDelegate::paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const {
-    QStyledItemDelegate::paint(painter, option, index);
+    QStyleOptionButton deleteButton;
+
+    int width = option.rect.width();
+    int height = option.rect.height();
+
+    deleteButton.rect = QRect(option.rect.left(), option.rect.top(), width, height);
+    deleteButton.text = "Delete";
+    deleteButton.state = QStyle::State_Enabled;
+
+    QApplication::style()->drawControl(QStyle::CE_PushButton, &deleteButton, painter);
 }
 
 bool ActionButtonsDelegate::editorEvent(QEvent *event, QAbstractItemModel *model, const QStyleOptionViewItem &option,
                                         const QModelIndex &index) {
-    return QStyledItemDelegate::editorEvent(event, model, option, index);
+    if (event->type() == QEvent::MouseButtonRelease) {
+        QModelIndex titleIndex = index.sibling(index.row(), PublicationTableModel::Columns::COL_TITLE);
+        std::string title = model->data(titleIndex).toString().toStdString();
+        emit deleteClicked(title);
+    }
+
+    return true;
 }
