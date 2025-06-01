c10ab066170051eaafffb50275a49288d9784c85
Alex David <davidalexrobertyt@gmail.com>
2025-06-01 23:37:02 +0300

    Move function implementations to source file

--- ui/DateEditDelegate.cpp
+++ ui/DateEditDelegate.cpp
@@ -3,3 +3,23 @@
 //
 
 #include "DateEditDelegate.h"
+
+
+QWidget *
+DateEditDelegate::createEditor(QWidget *parent, const QStyleOptionViewItem &option, const QModelIndex &index) const {
+    auto *editor = new QDateEdit(parent);
+    editor->setDisplayFormat("dd/MM/yyyy");
+    editor->setCalendarPopup(true);
+    return editor;
+}
+
+void DateEditDelegate::setEditorData(QWidget *editor, const QModelIndex &index) const {
+    QDateEdit *dateEdit = qobject_cast<QDateEdit *>(editor);
+    dateEdit->setDate(QDate::fromString(index.data().toString(), "dd/MM/yyyy"));
+}
+
+
+void DateEditDelegate::setModelData(QWidget *editor, QAbstractItemModel *model, const QModelIndex &index) const {
+    QDateEdit *dateEdit = qobject_cast<QDateEdit *>(editor);
+    model->setData(index, dateEdit->date().toString("dd/MM/yyyy"), Qt::EditRole);
+}
--- ui/DateEditDelegate.h
+++ ui/DateEditDelegate.h
@@ -14,22 +14,11 @@ public:
     explicit DateEditDelegate(QObject *parent = nullptr) : QStyledItemDelegate(parent) {}
 
     QWidget *
-    createEditor(QWidget *parent, const QStyleOptionViewItem &option, const QModelIndex &index) const override {
-        auto *editor = new QDateEdit(parent);
-        editor->setDisplayFormat("dd/MM/yyyy");
-        editor->setCalendarPopup(true);
-        return editor;
-    }
+    createEditor(QWidget *parent, const QStyleOptionViewItem &option, const QModelIndex &index) const override;
 
-    void setEditorData(QWidget *editor, const QModelIndex &index) const override {
-        QDateEdit *dateEdit = qobject_cast<QDateEdit *>(editor);
-        dateEdit->setDate(QDate::fromString(index.data().toString(), "dd/MM/yyyy"));
-    }
+    void setEditorData(QWidget *editor, const QModelIndex &index) const override;
 
-    void setModelData(QWidget *editor, QAbstractItemModel *model, const QModelIndex &index) const override {
-        QDateEdit *dateEdit = qobject_cast<QDateEdit *>(editor);
-        model->setData(index, dateEdit->date().toString("dd/MM/yyyy"), Qt::EditRole);
-    }
+    void setModelData(QWidget *editor, QAbstractItemModel *model, const QModelIndex &index) const override;
 
 };
 
